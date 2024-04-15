1c235698e456460bf42f105264c52688d23f8da7
Gustavo Dalboni <106346249+Gudalboni@users.noreply.github.com>
2024-04-15 00:26:30 -0300

    Delete Corrida.py

--- Corrida.py
+++
@@ -1,7 +0,0 @@
-class Corrida:
-
-    def __init__(nota, distancia, valor, passageiro, self):
-        self.nota = nota
-        self.distancia = distancia
-        self.valor = valor
-        self.passageiro = passageiro
